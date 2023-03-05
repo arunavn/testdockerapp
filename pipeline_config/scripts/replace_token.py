1af66698818953788380985795524361245b3b95
Arunav Narayan <arunavnarayan@gmail.com>
2023-03-05 18:40:55 +0530

    Update replace_token.py

--- pipeline_config/scripts/replace_token.py
+++ pipeline_config/scripts/replace_token.py
@@ -7,7 +7,7 @@ with open('./pipeline_config/configs/pipeline_config.json', 'r') as f:
 def replacetext(s_regex, file_path):
 	global CONFIG_DICT
 	# Opening the file in read and write mode
-	with open('SampleFile.txt','r+') as f:
+	with open('file_path','r+') as f:
 		file = f.read()
 		# Replacing the pattern with the string in the file data
 		file = s_regex.sub(lambda m: tokens[m.group(0)], file)
