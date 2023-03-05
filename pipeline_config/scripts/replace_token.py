def1c7ab225b245acab72999fa19c9119f377332
Arunav Narayan <arunavnarayan@gmail.com>
2023-03-05 18:45:18 +0530

    Update replace_token.py

--- pipeline_config/scripts/replace_token.py
+++ pipeline_config/scripts/replace_token.py
@@ -4,13 +4,12 @@ import os
 with open('./pipeline_config/configs/pipeline_config.json', 'r') as f:
 	CONFIG_DICT = json.load(f)
 
-def replacetext(s_regex, file_path):
-	global CONFIG_DICT
+def replacetext(s_regex, file_path, replace_dicts):
 	# Opening the file in read and write mode
 	with open(file_path,'r+') as f:
 		file = f.read()
 		# Replacing the pattern with the string in the file data
-		file = s_regex.sub(lambda m: tokens[m.group(0)], file)
+		file = s_regex.sub(lambda m: replace_dicts[m.group(0)], file)
 		f.seek(0)
 		f.write(file)
 		f.truncate()
@@ -22,7 +21,7 @@ def main():
 	tokens = CONFIG_DICT['test']['config_dict']
 	s_regex=re.compile("|".join([r"#<{}>#".format(t) for t in tokens]))
 	for f_path in CONFIG_DICT['replaceFileList']:
-		replacetext(s_regex= s_regex, file_path= f_path)
+		replacetext(s_regex= s_regex, file_path= f_path, replace_dicts= tokens)
 	  
 
 if __name__ == '__main__':
