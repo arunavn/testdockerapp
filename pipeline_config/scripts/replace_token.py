90e037eea1ce6012e802ad8cfa3bd19ab2305d0b
Arunav Narayan <arunavnarayan@gmail.com>
2023-03-05 17:45:55 +0530

    added python file

---
+++ pipeline_config/scripts/replace_token.py
@@ -0,0 +1,26 @@
+import re
+import json
+impor os
+CONFIG_DICT = {}
+
+def replacetext(s_regex, file_path):
+  global CONFIG_DICT
+	# Opening the file in read and write mode
+	with open('SampleFile.txt','r+') as f:
+		file = f.read()
+		# Replacing the pattern with the string in the file data
+		file = re.sub(search_text, replace_text, file)
+		f.seek(0)
+		f.write(file)
+		f.truncate()
+    
+def main():
+  print(f"cwd: {os.getcwd()}")
+  tokens = {'hello': 'world'}
+  s_regex=re.compile("|".join([r"#<{}>".format(t) for t in tokens]))
+  
+  
+
+if __name__ = '__main__':
+  main()
+ 
