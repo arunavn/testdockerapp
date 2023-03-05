00ce8810c21eafc5c1e5341be569292308d7f659
Arunav Narayan <arunavnarayan@gmail.com>
2023-03-05 18:35:45 +0530

    Update replace_token.py

--- pipeline_config/scripts/replace_token.py
+++ pipeline_config/scripts/replace_token.py
@@ -10,7 +10,7 @@ def replacetext(s_regex, file_path):
 	with open('SampleFile.txt','r+') as f:
 		file = f.read()
 		# Replacing the pattern with the string in the file data
-		file = re.sub(search_text, replace_text, file)
+		file = s_regex.sub(lambda m: tokens[m.group(0)], file)
 		f.seek(0)
 		f.write(file)
 		f.truncate()
@@ -19,11 +19,11 @@ def main():
 	global CONFIG_DICT
 	print(f"cwd: {os.getcwd()}")
 	print(CONFIG_DICT)
-	tokens = {'hello': 'world'}
-	s_regex=re.compile("|".join([r"#<{}>".format(t) for t in tokens]))
-	
-  
-  
+	tokens = CONFIG_DICT['test']['config_dict']
+	s_regex=re.compile("|".join([r"#<{}>#".format(t) for t in tokens]))
+	for f_path in CONFIG_DICT['replaceFileList']:
+		replacetext(s_regex= s_regex, file_path= f_path)
+	  
 
 if __name__ == '__main__':
 	main()
